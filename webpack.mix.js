406a31fa7430bca9474c6eba058e306b1cc55b7f
RinaHariyanti <rinayanti1771@gmail.com>
2022-02-20 20:28:00 +0700

    Laravel dan Tailwind Css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss')
         //
     ]);
 
